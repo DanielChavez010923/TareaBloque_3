68c5feb5d51e42bb114ce82481f43abd421b8b67
DanielChavez010923 <163219796+DanielChavez010923@users.noreply.github.com>
2024-04-04 23:18:49 +0000

    agregar la direccion absoluta

--- Tarea3.sh
+++ Tarea3.sh
@@ -14,29 +14,29 @@ done < ventas.csv
 
 # Generar reporte
 > reporte_ventas.txt
-echo "-------------------------------------------------" >> reporte_ventas.txt
+echo "-------------------------------------------------" >> /workspaces/TareaBloque_3/reporte_ventas.txt
 echo "Total venta mensual" >> reporte_ventas.txt
-echo "-------------------------------------------------" >> reporte_ventas.txt
+echo "-------------------------------------------------" >> /workspaces/TareaBloque_3/reporte_ventas.txt
 for ((i=0; i<${#venta_mensual[@]}; i++)) do
-    echo " ${mes[$i]}" "$" "${venta_mensual[$i]}" >> reporte_ventas.txt        
+    echo " ${mes[$i]}" "$" "${venta_mensual[$i]}" >> workspaces/TareaBloque_3/reporte_ventas.txt        
 done
-echo "" >> reporte_ventas.txt
+echo "" >> workspaces/TareaBloque_3/reporte_ventas.txt
 
-echo "-----------------------------------------------" >> reporte_ventas.txt
+echo "-----------------------------------------------" >> workspaces/TareaBloque_3/reporte_ventas.txt
 producto_mas_vendido=$(cut -d';' -f3 ventas.csv | sort | uniq -c | sort -nr | head -n1) 
-echo -e "El producto más vendido es:\n-----------------------------------------------\n $(echo $producto_mas_vendido | awk '{$1=""; print $0}' | sed 's/^ //')" >> reporte_ventas.txt
-echo "" >> reporte_ventas.txt
+echo -e "El producto más vendido es:\n-----------------------------------------------\n $(echo $producto_mas_vendido | awk '{$1=""; print $0}' | sed 's/^ //')" >> workspaces/TareaBloque_3/reporte_ventas.txt
+echo "" >> workspaces/TareaBloque_3/reporte_ventas.txt
 
-echo "-----------------------------------------------" >> reporte_ventas.txt
-echo "Monto anual" >> reporte_ventas.txt
-echo "-----------------------------------------------" >> reporte_ventas.txt
+echo "-----------------------------------------------" >> workspaces/TareaBloque_3/reporte_ventas.txt
+echo "Monto anual" >> workspaces/TareaBloque_3/reporte_ventas.txt
+echo "-----------------------------------------------" >> workspaces/TareaBloque_3/reporte_ventas.txt
 for i in "${!venta_mensual[@]}"; do
     monto_anual=$((monto_anual + ${venta_mensual[$i]}))
 done
-echo " $""$monto_anual" >> reporte_ventas.txt
-echo "" >> reporte_ventas.txt
+echo " $""$monto_anual" >> workspaces/TareaBloque_3/reporte_ventas.txt
+echo "" >> workspaces/TareaBloque_3/reporte_ventas.txt
 
-echo "-----------------------------------------------" >> reporte_ventas.txt
+echo "-----------------------------------------------" >> workspaces/TareaBloque_3/reporte_ventas.txt
 cliente_mas_frecuente=$(cut -d';' -f4 ventas.csv | sort | uniq -c | sort -nr | head -n1) 
-echo -e "El cliente más frecuente es:\n-----------------------------------------------\n $(echo $cliente_mas_frecuente | awk '{$1=""; print $0}' | sed 's/^ //')" >> reporte_ventas.txt
-echo "" >> reporte_ventas.txt
+echo -e "El cliente más frecuente es:\n-----------------------------------------------\n $(echo $cliente_mas_frecuente | awk '{$1=""; print $0}' | sed 's/^ //')" >> workspaces/TareaBloque_3/reporte_ventas.txt
+echo "" >> workspaces/TareaBloque_3/reporte_ventas.txt
